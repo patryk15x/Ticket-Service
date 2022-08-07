221e57a5a3b8e5f0df981e40d0ebd7b3fd1b2429
Patryk Szaruga <szarugapatryk@gmail.com>
2022-08-07 21:34:10 +0200

    add ticket entity

---
+++ src/main/java/com/ticketservice/ticketservice/model/Ticket.java
@@ -0,0 +1,31 @@
+package com.ticketservice.ticketservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
